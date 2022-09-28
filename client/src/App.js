9c261a87d0611c39ea8100d2f3a686ac53ca68c1
Shahab Hesaraki <Shahabhes@gmail.com>
2022-09-28 12:50:09 -0400

    Ready to go

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route></Route>
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default App;
